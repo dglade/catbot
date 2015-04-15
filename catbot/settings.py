1dd1d3bcab437276dd62d6d87dcf00942a02f821
dev074 <dan.glade>
2015-04-15 08:11:08 -0500

    Initial check-in

---
+++ catbot/settings.py
@@ -0,0 +1,24 @@
+# hipchat api v1 token
+ADMIN_API_TOKEN = 'YOUR TOKEN'
+
+# name to send messages under
+SENDER_NAME = 'catbot'
+
+# list of messages to convey
+MESSAGE_LIST = ['meow', 'purr']
+
+# message color (yellow, red, green, purple, gray, or random)
+MESSAGE_COLOR = 'random'
+
+# message format (text or html)
+MESSAGE_FORMAT = 'text'
+
+# string list of hipchat usernames to follow
+USERS_TO_FOLLOW = ['catbot', 'User One', 'User Two']
+
+# integer list of room API IDs to operate under
+ROOMS_LIST = ['ROOM ID']
+
+# integer sleep interval
+SLEEP_INTERVAL = 3 
+
